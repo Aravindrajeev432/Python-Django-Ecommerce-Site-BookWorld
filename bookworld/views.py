d27496b98acfaa7a41c9625c728d090718e1527c
Aravind R <ndarvi48332@gmail.com>
2022-07-28 15:54:47 +0530

    login page and register page built

--- bookworld/views.py
+++ bookworld/views.py
@@ -8,6 +8,35 @@ from django.views.decorators.cache import cache_control
 def index(request):
     return render(request,'landing.html')
 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def login(request):
-    log="login"
-    return render(request,'login.html',{'l':log})
+    if request.method == 'POST':
+       email = request.POST['email']
+       password = request.POST['pass']
+       print(email,password)
+       if email=="" or password == "":
+           print("fail1")
+           return JsonResponse(
+               {'success':False
+               },
+               safe=False
+           )
+    
+    return render(request,'login.html')
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def register(request):
+    
+    if request.method == 'POST':
+        fname = request.POST.get('firstname')
+        sname = request.POST.get('secondname')
+        phone = request.POST.get('phone')
+        email =  request.POST.get('email')
+        password =  request.POST.get('password1')
+        print("first name-"+fname)
+        print("second name -"+sname)
+        print("Phone-"+str(phone))
+        print("email-"+email)
+        print("password-"+password)
+    
+    return render(request,'register.html')
